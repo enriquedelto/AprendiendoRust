80086cef08c88ff0a91e8cd4b8e337c280e8e083
enriquedelto <enriquedelto@gmail.com>
2024-04-12 14:22:26 +0100

    esqueleto innecesario

--- ejercicios/hola_mundo/target/debug/deps/hola_mundo.d
+++
@@ -1,5 +0,0 @@
-C:\Users\EnriqueDelto\Desktop\VSCode\AprendiendoRust\ejercicios\hola_mundo\target\debug\deps\hola_mundo.exe: src\main.rs
-
-C:\Users\EnriqueDelto\Desktop\VSCode\AprendiendoRust\ejercicios\hola_mundo\target\debug\deps\hola_mundo.d: src\main.rs
-
-src\main.rs:
--- ejercicios/hola_mundo/target/debug/hola_mundo.d
+++
@@ -1,1 +0,0 @@
-C:\Users\EnriqueDelto\Desktop\VSCode\AprendiendoRust\ejercicios\hola_mundo\target\debug\hola_mundo.exe: C:\Users\EnriqueDelto\Desktop\VSCode\AprendiendoRust\ejercicios\hola_mundo\src\main.rs
